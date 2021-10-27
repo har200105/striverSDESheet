2ae5c6b2e83b02e88f7535893aaade8f97575a0b
geekySapien <escaping.blurred.reality@gmail.com>
2021-10-27 13:07:07 +0530

    Added some comments in mergeIntervals.cpp

--- Arrays/mergeIntervals.cpp
+++ Arrays/mergeIntervals.cpp
@@ -5,11 +5,22 @@
         }
         sort(intervals.begin(), intervals.end());
         vector<int> tempInterval = intervals[0]; 
-        
+     
         for(auto it : intervals) {
+
+            //consider the case  
+            // tempInterval->[1,8],
+            // it-> [6,7]
+            //here it[0]<tempInterval[1]
+            // so we make tempInterval[1] equal to it[1]
             if(it[0] <= tempInterval[1]) {
                 tempInterval[1] = max(it[1], tempInterval[1]); 
-            } else {
+            }
+            // else we push the tempInterval in this case
+            // tempInterval->[1,6],
+            // it-> [7, 9]
+
+             else {
                 mergedIntervals.push_back(tempInterval); 
                 tempInterval = it; 
             }
